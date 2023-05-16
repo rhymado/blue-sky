591ca61f6743fdf4d88537a75d0e60d40f9c87be
rhymaado <mightydhiyamatra@yahoo.co.id>
2023-05-16 12:58:59 +0700

    notification

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ import {AppRegistry} from 'react-native';
 // import Home from './Home';
 import Router from './router';
 import {name as appName} from './app.json';
+// import notifee from '@notifee/react-native';
+
+// notifee.onBackgroundEvent(async () => {
+//   console.log('background notifee');
+// });
 
 AppRegistry.registerComponent(appName, () => Router);
--- src/screens/Content/dashboard.js
+++ src/screens/Content/dashboard.js
@@ -1,9 +1,81 @@
 import {View, Text, Pressable} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
+import notifee, {AndroidImportance} from '@notifee/react-native';
+import messaging from '@react-native-firebase/messaging';
+import axios from 'axios';
 
 const Dashboard = () => {
   const navigation = useNavigation();
+  const [token, setToken] = useState('');
+  const onSendNotification = async (title = 'judul', body = 'konten') => {
+    // console.log(title, body);
+    try {
+      // permission (IOS)
+      await notifee.requestPermission();
+
+      // create channel
+      await notifee.createChannel({
+        id: 'normal',
+        name: 'Normal Notification',
+        sound: 'default',
+      });
+      await notifee.createChannel({
+        id: 'urgent',
+        name: 'high_priority',
+        sound: 'default',
+        importance: AndroidImportance.HIGH,
+      });
+      // console.log(normalChannelId);
+      // console.log(await notifee.getChannels());
+
+      // display notification
+      await notifee.displayNotification({
+        android: {
+          channelId: 'urgent',
+        },
+        title,
+        subtitle: 'subjudul',
+        body,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const onCreateFCMToken = async () => {
+    try {
+      // register device
+      if (!messaging().isDeviceRegisteredForRemoteMessages)
+        await messaging().registerDeviceForRemoteMessages();
+
+      // register token
+      const token = await messaging().getToken();
+      setToken(token);
+      console.log('FCM TOKEN ==>', token);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const getNotificationFromAPI = async () => {
+    const baseUrl = 'http://192.168.100.17:8080';
+    await axios
+      .post(`${baseUrl}/notification`, {token})
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    const onMessageReceived = async remoteMessage => {
+      // console.log('FCM Payload ==>', remoteMessage);
+      const {title, body} = remoteMessage.notification;
+      onSendNotification(title, body);
+    };
+    messaging().onMessage(onMessageReceived);
+    messaging().setBackgroundMessageHandler(onMessageReceived);
+  }, []);
   return (
     <View>
       <Text>Dashboard</Text>
@@ -18,6 +90,44 @@ const Dashboard = () => {
           alignItems: 'center',
         }}>
         <Text>Go to Home</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => onSendNotification()}
+        style={{
+          padding: 5,
+          borderWidth: 2,
+          borderColor: 'black',
+          borderRadius: 5,
+          margin: 5,
+          alignItems: 'center',
+          backgroundColor: 'black',
+        }}>
+        <Text style={{color: 'white'}}>Trigger Notification</Text>
+      </Pressable>
+      <Pressable
+        onPress={onCreateFCMToken}
+        style={{
+          padding: 5,
+          borderWidth: 2,
+          borderColor: 'black',
+          borderRadius: 5,
+          margin: 5,
+          alignItems: 'center',
+        }}>
+        <Text>Register to FCM</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => getNotificationFromAPI()}
+        style={{
+          padding: 5,
+          borderWidth: 2,
+          borderColor: 'black',
+          borderRadius: 5,
+          margin: 5,
+          alignItems: 'center',
+          backgroundColor: 'black',
+        }}>
+        <Text style={{color: 'white'}}>Trigger Notification From Server</Text>
       </Pressable>
     </View>
   );
